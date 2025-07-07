56b9d629be4e1df2ad9317488afbb5061b541aac
kiteminimal <mohdshoyeb189@gmail.com>
2025-07-07 22:02:45 +0530

    refactor: clean up Pokemon component and improve API fetching logic

--- PokenmonAPI/src/Pokemon.jsx
+++ PokenmonAPI/src/Pokemon.jsx
@@ -1,50 +1,68 @@
 import React, { useEffect, useState } from "react";
-import "./index.css"
-
+import "./index.css";
 
 const Pokemon = () => {
+  const [pokemonName, setPokemonName] = useState([]);
+  const [pokemonImage, setPokemonImage] = useState("");
 
-    const [pokemonName, setPokemonName] = useState("");
-    const [pokemonImage, setPokemonImage] = useState("");
+  const apiUrl = "https://pokeapi.co/api/v2/pokemon?limit=30";
 
-    const apiUrl = "https://pokeapi.co/api/v2/pokemon?limit=30";
-
-    const fetchPokemonAPI = async () => {
-        await fetch(apiUrl)
-        .then(res => {
-            const data = res.json();
-            return data;    
-        })
-        .then(data => {
-            const result = data.results;
-            console.log(result);
-            return result;
-        })
-        .then( (results) => {
-            results.forEach(async (pokemon, idx) => {
-                const pokemonUrl = pokemon.url;
-                await fetch(pokemonUrl)
-                .then((res) => {
-                    const data = res.json()
-                    return data;
-                })
-                .then((data) => {
-                    // console.log(data)
-                    
-                })
+  const fetchPokemonAPI = async () => {
+    await fetch(apiUrl)
+      .then((res) => {
+        const data = res.json();
+        return data;
+      })
+      .then((data) => {
+        const result = data.results;
+        // console.log(result);
+        return result;
+      })
+      .then((results) => {
+        results.map(async (pokemon, idx) => {
+          const pokemonUrl = pokemon.url;
+          await fetch(pokemonUrl)
+            .then((res) => {
+              const data = res.json();
+              return data;
             })
-        })
-    }
+            .then((data) => {
+              // console.log(data)
+              setPokemonName((prev) => {
+                return [...prev, data];
+              });
+            });
+        });
+      });
+  };
 
-    useEffect(() => {
-        fetchPokemonAPI();
-    })
-    return ( 
-        <div>
-            <h1>Welcome to the Pokenmon API</h1>
-            <p>Explore the world of Pokenmon!</p>
-        </div>
-    )
-}
+  useEffect(() => {
+    fetchPokemonAPI();
+  }, []);
 
-export default Pokemon
+  return (
+    <section className="container">
+      <header>
+        <h1>Let's Catch Pokemon</h1>
+      </header>
+      <div style={{ margin: "2rem" }}>
+        <ul className="cards">
+          {pokemonName.map((pokemon, idx) => {
+            return (
+              <li key={idx} className="pokemon-card">
+                <figure>
+                  <img className="pokemon-image" src={pokemon.sprites.front_default} alt={pokemon.name} />
+                  <h2>{pokemon.name}</h2>
+                  <p>Height: {pokemon.height}</p>
+                  <p>Weight: {pokemon.weight}</p>
+                </figure>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default Pokemon;
